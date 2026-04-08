9da969aec5b67f1bfbf6b197c49111b6cd181a3a
mdisailor <mdi_sailor@tin.it>
2026-04-08 15:31:23 +0200

    Update engine.js
    x

--- api/engine.js
+++ api/engine.js
@@ -1,5 +1,5 @@
 module.exports = async function handler(req, res) {
-res.setHeader(“Access-Control-Allow-Origin”, “*”);
+res.setHeader('Access-Control-Allow-Origin', '*');
 return res.status(200).json({ ok: true, v: “test-minimal”, ts: Date.now() });
 };
 // Fine codice - test
